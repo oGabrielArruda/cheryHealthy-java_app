efcdbf2971e1a77356ffe7092f13eb6a1b24a98a
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-09-19 08:17:41 -0300

    start janela 1

--- src/programa/janela1.java
+++ src/programa/janela1.java
@@ -3,10 +3,18 @@ package programa;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class janela1 {
 
 	private JFrame frame;
+	private JTextField txtId;
+	private JTextField txtSenha;
 
 	/**
 	 * Launch the application.
@@ -36,8 +44,40 @@ public class janela1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 392, 229);
+		frame.setBounds(100, 100, 659, 449);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblId = new JLabel("Id");
+		lblId.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 14));
+		lblId.setBounds(198, 136, 91, 14);
+		frame.getContentPane().add(lblId);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+		lblSenha.setBounds(161, 172, 91, 14);
+		frame.getContentPane().add(lblSenha);
+		
+		txtId = new JTextField();
+		txtId.setBounds(262, 135, 152, 20);
+		frame.getContentPane().add(txtId);
+		txtId.setColumns(10);
+		
+		txtSenha = new JTextField();
+		txtSenha.setColumns(10);
+		txtSenha.setBounds(262, 171, 152, 20);
+		frame.getContentPane().add(txtSenha);
+		
+		JButton btnLogar = new JButton("Entrar");
+		btnLogar.setBounds(298, 211, 89, 23);
+		frame.getContentPane().add(btnLogar);
+		
+		JLabel lblCadastro = new JLabel("N\u00E3o tem uma conta? ");
+		lblCadastro.setBounds(219, 332, 101, 14);
+		frame.getContentPane().add(lblCadastro);
+		
+		JButton btnGoCadastro = new JButton("Cadastrar-se");
+		btnGoCadastro.setBounds(330, 328, 101, 23);
+		frame.getContentPane().add(btnGoCadastro);
 	}
-
 }
